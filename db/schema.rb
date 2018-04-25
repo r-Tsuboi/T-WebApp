a313e5af29f2f2c88f80eed70f97b33975f825b3
vagrant <vagrant@localhost.localdomain>
2018-04-25 02:19:33 +0000

    投稿テーブルにカラムを再追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425012732) do
+ActiveRecord::Schema.define(version: 20180425013234) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.string "post_image"
   end
 
   create_table "users", force: :cascade do |t|
