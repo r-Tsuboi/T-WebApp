21a6393c2be5f98e3695576de0db9e1d3d86a740
r-tsuboi <r-tsuboi@allmedia.co.jp>
2018-05-14 00:58:08 +0000

    検索機能の完成

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -4,25 +4,22 @@ class SearchsController < ApplicationController
     @tags = Tag.all
   end
 
-#未完成-----------ここから-----------------------
   def search
     @word = params[:tag_name]
 
-    #空の検索を防ぎたい
-    if @word == nil
+    if params[:tag_name].blank?
       redirect_to("/searchs/index")
       flash[:notice] = "文字を入力してください"
-    end
 
-    if @tag = Tag.find_by("tag_name like '%" + @word + "%'")
+    elsif @tag = Tag.find_by("tag_name like '%" + @word + "%'")
       redirect_to("/searchs/#{params[:tag_name]}")
+
     else
       redirect_to("/searchs/index")
       flash[:notice] = "検索結果がありませんでした"
     end
 
   end
-#未完成-----------ここまで-----------------------
 
   def result
     @word = params[:tag_name]
